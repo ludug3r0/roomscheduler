1820322f5af9f2e0bb0089af96132418ad0e9620
Rafael Oliveira <ludug3r0@gmail.com>
2014-01-27 17:00:57 -0300

    Fetching and rendering from Google Calendar API. Discarded RESTAdapter.

---
+++ js/src/app-config.js
@@ -0,0 +1,11 @@
+app_config = (function() {
+    //FILL THIS
+    var apiKey = "AIzaSyCwF1EWlox3eiV1Z47MfC58D3-qEO6DWpk";
+    var clientId = "813569615001-pqsv493rier46gimfboohhu24dtj7tm8.apps.googleusercontent.com";
+    //END
+
+    var ret = {};
+    ret.apiKey = apiKey;
+    ret.clientId = clientId;
+    return ret;
+})();
--- js/src/google-api-auth.js
+++ js/src/google-api-auth.js
@@ -1,9 +1,5 @@
-var hardCodedCalendarId = 'primary';
-
-var clientId = 'aaaaaaa';
-
-var apiKey = 'XXXXXX';
-
+var clientId = app_config.clientId;
+var apiKey = app_config.apiKey;
 var scopes = 'https://www.googleapis.com/auth/calendar';
 
 function handleClientLoad() {
@@ -16,11 +12,10 @@ function checkAuth() {
 }
 
 function handleAuthResult(authResult) {
-    console.log(authResult);
     var authorizeButton = document.getElementById('authorize-button');
     if (authResult && !authResult.error) {
         authorizeButton.style.visibility = 'hidden';
-        makeApiCall();
+        initApp();
     } else {
         authorizeButton.style.visibility = '';
         authorizeButton.onclick = handleAuthClick;
@@ -30,28 +25,4 @@ function handleAuthResult(authResult) {
 function handleAuthClick(event) {
     gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
     return false;
-}
-
-function makeApiCall() {
-    gapi.client.load('calendar', 'v3', function() {
-        var request = gapi.client.calendar.events.list({
-            'calendarId': 'primary',
-            'timeMin': new Date(),
-            'singleEvents': true,
-            'orderBy': 'startTime'
-        });
-        console.log(request);
-
-        request.execute(function(resp) {
-            console.log(resp);
-            for (var i = 0; i < resp.items.length; i++) {
-                var li = document.createElement('li');
-                var eventDate = resp.items[i].start.dateTime;
-                var eventSummary = resp.items[i].summary;
-                var eventText = [eventDate, eventSummary].join(' - ');
-                li.appendChild(document.createTextNode(eventText));
-                document.getElementById('events').appendChild(li);
-            }
-        });
-    });
 }
