9e8afd591078478569f197cc5cb6efc6ad303f6d
Henrique Ribeiro Rezende <henriquerrezende@gmail.com>
2014-02-05 19:11:27 -0200

    Starting from the scratch with a top down approach

--- js/src/app.js
+++ js/src/app.js
@@ -5,191 +5,74 @@ function initApp() {
     });
 
     App.Router.map(function () {
-        this.resource('calendar', { path: '/calendars/:calendar_id' });
-        this.resource('event', { path: '/events/:event_id' })
-    });
-
-    App.ApplicationAdapter = DS.Adapter.extend({
-        find: function(store, type, id) {
-            return new Ember.RSVP.Promise(function (resolve, reject) {
-                type.sync.find(id, resolve, reject);
+        this.resource("calendar", { path: "/:calendar_id" }, function() {
+            this.resource("events", { path: "/" }, function() {
+                this.resource("event", { path: "/:event_id" }, function() {
+                    this.route("edit", { path: "/edit" });
+                });
+                this.route("new", { path: "/new" });
             });
-        },
-
-        findAll: function(store, type, sinceToken) {
-            console.log("AppAdapter.findAll");
-            return new Ember.RSVP.Promise(function (resolve, reject) {
-                type.sync.findAll(id, resolve, reject);
-            });
-        }
+        });
     });
 
-    var attr = DS.attr, hasMany = DS.hasMany, belongsTo = DS.belongsTo;
-
-    App.Calendar = DS.Model.extend({
-        summary: attr('string'),
-        timeZone: attr('string'),
-
-        events: hasMany('event', {async: true})
-    });
-
-    App.Event = DS.Model.extend({
-        summary: attr('string'),
-        description: attr('string'),
-        location: attr('string'),
-        start: attr('dateTime'),
-        end: attr('dateTime'),
-
-        calendar: belongsTo('calendar', {async: true})
-    });
-
-    App.RoomRoute = Ember.Route.extend({
+    App.CalendarRoute = Ember.Route.extend({
         model: function (params) {
-            console.log('NAO PODE');
-            return new Ember.RSVP.Promise(function (resolve, reject) {
-                gapi.client.load('calendar', 'v3', function () {
-                    var request = gapi.client.calendar.events.list({
-                        'calendarId': params.room_id,
-                        'timeMin': new Date(),
-                        'timeMax': (function () {
-                            var dt = new Date();
-                            dt.setDate(dt.getDate() + 7);
-                            return dt;
-                        })(),
-                        'singleEvents': true,
-                        'orderBy': 'startTime'
-                    });
-
-                    request.execute(function (resp) {
-                        if (!resp || resp.error) {
-                            Ember.run(null, reject, resp.error);
-                        } else {
-                            Ember.run(null, resolve, resp);
-                        }
-                    });
-                });
-            });
-        },
-        setupController: function (controller, model) {
-            console.log('NAO PODE MESMO');
-            model.items.forEach(function (item) {
-                var startDate = item.start.date || item.start.dateTime;
-                var endDate = item.end.date || item.end.dateTime;
-                //TODO: format startDate
-                item.startDate = new Date(startDate).toString('dddd, MMMM d');
-                item.endDate = new Date(endDate).toString('MMMM d');
-                item.startTime = new Date(startDate).toString('HH:mm');
-                item.endTime = new Date(endDate).toString('HH:mm');
-            });
-
-            var days = model.items.reduce(function (days, event) {
-                if (days[days.length - 1] && event.startDate === days[days.length - 1].events[0].startDate) {
-                    days[days.length - 1].events.push(event);
-                } else {
-                    days.push({description: event.startDate, events: [event]})
-                }
-                if (event.summary === undefined) {
-                    event.summary = '(no title)'
-                }
-                return days;
-            }, []);
-
-            controller.set('model', {
-                summary: model.summary,
-                days: days
-            });
+            //Gerar menu
+            console.log('CalendarRoute');
+            return {
+                title: 'Pagu'
+            };
         }
     });
 
-    App.Calendar.sync = {
-        find: function(id, resolve, reject) {
-            gapi.client.load('calendar', 'v3', function () {
-                var request = gapi.client.calendar.calendars.get({
-                    'calendarId': id
-                });
-
-                request.execute(function (resp) {
-                    if (!resp || resp.error) {
-                        console.log("App.Calendar.sync.find.error");
-                        console.log(resp.error);
-                        Ember.run(null, reject, resp.error);
-                    } else {
-                        console.log("App.Calendar.sync.find.success");
-                        console.log(resp);
-                        Ember.run(null, resolve, resp);
-                    }
-                });
-            })
-        }
-    };
-
-    App.Event.sync = {
-        findAll: function(resolve, reject) {
-            gapi.client.load('calendar', 'v3', function () {
-                var request = gapi.client.calendar.event.list({
-                    'calendarId': id
-                });
-
-                request.execute(function (resp) {
-                    if (!resp || resp.error) {
-                        console.log("App.Event.sync.find.error");
-                        console.log(resp.error);
-                        Ember.run(null, reject, resp.error);
-                    } else {
-                        console.log("App.Event.sync.find.success");
-                        console.log(resp);
-                        Ember.run(null, resolve, resp);
-                    }
-                });
-            })
-        }
-    };
-
-    App.EventNewRoute = Ember.Route.extend({
+    App.EventsIndexRoute = Ember.Route.extend({
         model: function (params) {
-            console.log('laksjdlajsd');
-            console.log(params);
-            return new Ember.RSVP.Promise(function (resolve, reject) {
-//                gapi.client.load('calendar', 'v3', function () {
-//                    var request = gapi.client.calendar.events.insert({
-//                        'calendarId': params.room_id,
-//                        'resource': {
-//                            'end': {
-//                                dateTime: '2014-02-03T20:30:00-03:00'
-//                            },
-//                            'start': {
-//                                dateTime: '2014-02-03T19:30:00-03:00'
-//                            }
-//                        }
-//                    });
-//
-//                    request.execute(function (resp) {
-//                        if (!resp || resp.error) {
-//                            Ember.run(null, reject, resp.error);
-//                        } else {
-//                            Ember.run(null, resolve, resp);
-//                        }
-//                    });
-//                });
-            });
+            //Listar eventos
+            console.log('EventsIndexRoute');
+            var model = this.modelFor('calendar');
+            model.events = ['event1', 'event2'];
+            return model;
         }
     });
-
-
-//    App.CalendarRoute = Ember.Route.extend({
-//        model: function (params) {
-//            console.log('CalendarRoute');
-//            console.log(params);
-//            return this.store.find('calendar', params.calendar_id);
-//        }
-//    });
 
     App.EventRoute = Ember.Route.extend({
         model: function (params) {
-            console.log('CalendarEventsRoute');
-            console.log(params);
-            return this.store.find('event');
-//            return this.store.find('calendar', params.calendar_id);
+            //Retorna o evento para edit
+            console.log('EventRoute');
+            return 'edit_event';
+        }
+    });
+
+    App.EventEditRoute = Ember.Route.extend({
+        model: function (params) {
+            //Abre formulário para editar evento
+            console.log('EventEditRoute');
+            this.modelFor('calendar').is_editing = true;
+            return this.modelFor('event');
+        }
+    });
+
+    App.EventsNewRoute = Ember.Route.extend({
+        model: function (params) {
+            //Abre formulário para criar o evento
+            console.log('EventsNewRoute');
+            return 'new_event';
+        }
+    });
+
+    App.EventEditController = Ember.ObjectController.extend({
+        actions: {
+            submitAction : function() {
+                console.log("now we can edit the model:" + this.get("model"));
+            }
+        }
+    });
+
+    App.EventsNewController = Ember.ObjectController.extend({
+        actions: {
+            submitAction : function() {
+                console.log("now we can add the model:" + this.get("model"));
+            }
         }
     });
 }
